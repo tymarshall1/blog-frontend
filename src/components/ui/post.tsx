ea9c9846a3b4b00f9e0c23de3188d41bdf7f2f9b
tymarshall1 <60017345+tymarshall1@users.noreply.github.com>
2024-03-19 18:33:57 -0400

    Added skeleton loading to post component

--- src/components/ui/post.tsx
+++ src/components/ui/post.tsx
@@ -1,4 +1,6 @@
 import { limitCharacters } from "@/lib/utils";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useState } from "react";
 
 type PostProps = {
   community: string;
@@ -12,47 +14,60 @@ type PostProps = {
 };
 
 function Post(props: PostProps) {
+  //will have to set this when content is loaded in the future
+  //for now just keeping set to true
+  const [loaded, setLoaded] = useState(true);
   return (
     <div className="max-w-screen-md p-3 mx-auto space-y-3 overflow-hidden text-black border-2 border-transparent rounded cursor-pointer bg-zinc-300 hover:border-secondary max-h-96">
-      <div className="flex items-center gap-1">
-        <span className="bg-black rounded-full w-7 h-7"></span>
-        <h2 className="font-normal text-md">{props.community}</h2>{" "}
-        <span className="text-sm font-normal opacity-50">
-          Posted by {props.user} {props.timeCreated} hours ago
-        </span>
-      </div>
-      <div>
-        <h3 className="text-xl font-semibold">{props.title}</h3>
-        <p className="font-light tracking-wide ">
-          {limitCharacters(props.body, 200)}
-        </p>
-      </div>
-      <div className="flex flex-wrap justify-between">
-        <div className="flex text-sm font-light">
-          <div className="flex items-center gap-1 p-1 rounded cursor-pointer hover:bg-secondary">
-            <span className="material-symbols-outlined">thumb_up</span>
-            <span>{props.likes}</span>
+      {loaded && (
+        <>
+          <div className="flex items-center gap-1">
+            <span className="bg-black rounded-full w-7 h-7"></span>
+            <h2 className="font-normal text-md">{props.community}</h2>{" "}
+            <span className="text-sm font-normal opacity-50">
+              Posted by {props.user} {props.timeCreated} hours ago
+            </span>
           </div>
-          <div className="flex items-center gap-1 p-1 rounded cursor-pointer hover:bg-secondary">
-            <span className="material-symbols-outlined">thumb_down</span>
-            <span>{props.dislikes}</span>
+          <div>
+            <h3 className="text-xl font-semibold">{props.title}</h3>
+            <p className="font-light tracking-wide ">
+              {limitCharacters(props.body, 200)}
+            </p>
           </div>
-        </div>
-        <div className="flex gap-2 text-sm font-light tracking-wide">
-          <div className="flex items-center gap-1 p-1 rounded cursor-pointer hover:bg-secondary">
-            <span className="material-symbols-outlined">forum</span>
-            <span>{props.comments} Comments</span>
+          <div className="flex flex-wrap justify-between">
+            <div className="flex text-sm font-light">
+              <div className="flex items-center gap-1 p-1 rounded cursor-pointer hover:bg-secondary">
+                <span className="material-symbols-outlined">thumb_up</span>
+                <span>{props.likes}</span>
+              </div>
+              <div className="flex items-center gap-1 p-1 rounded cursor-pointer hover:bg-secondary">
+                <span className="material-symbols-outlined">thumb_down</span>
+                <span>{props.dislikes}</span>
+              </div>
+            </div>
+            <div className="flex gap-2 text-sm font-light tracking-wide">
+              <div className="flex items-center gap-1 p-1 rounded cursor-pointer hover:bg-secondary">
+                <span className="material-symbols-outlined">forum</span>
+                <span>{props.comments} Comments</span>
+              </div>
+              <div className="flex items-center gap-1 p-1 rounded cursor-pointer hover:bg-secondary">
+                <span className="material-symbols-outlined">share</span>
+                <span>Share</span>
+              </div>
+              <div className="flex items-center gap-1 p-1 rounded cursor-pointer hover:bg-secondary">
+                <span className="material-symbols-outlined">bookmark</span>
+                <span>Save</span>
+              </div>
+            </div>
           </div>
-          <div className="flex items-center gap-1 p-1 rounded cursor-pointer hover:bg-secondary">
-            <span className="material-symbols-outlined">share</span>
-            <span>Share</span>
-          </div>
-          <div className="flex items-center gap-1 p-1 rounded cursor-pointer hover:bg-secondary">
-            <span className="material-symbols-outlined">bookmark</span>
-            <span>Save</span>
-          </div>
-        </div>
-      </div>
+        </>
+      )}
+      {!loaded && (
+        <>
+          <Skeleton className="h-[25px] w-full rounded-xl" />
+          <Skeleton className="h-[15rem] w-full rounded-xl" />
+        </>
+      )}
     </div>
   );
 }
